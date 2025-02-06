0fc3ea75014cc60e61b867e3654da2e6e2604135
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-02-07 00:09:47 +0200

    start accordion-js

--- src/js/aboutMe.js
+++ src/js/aboutMe.js
@@ -1,1 +1,12 @@
-console.log('about me');
+import Swiper from 'swiper';
+import Accordion from "accordion-js";
+
+new Accordion(".about-me-list", {
+    elementClass: 'about-me-item',
+    triggerClass: 'about-me-list-button',
+    panelClass: 'about-me-item-l',
+    showMultiple: true,
+});
+
+
+const swiper = document.querySelector('.about-me-list-button');
