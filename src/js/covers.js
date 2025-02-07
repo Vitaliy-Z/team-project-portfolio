74914a3d6ea35ec00a45380fd56cbf97193e4dae
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-02-07 22:58:32 +0000

    html+css

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,4 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const coversSection = document.querySelector('#covers');
 
+    if (!coversSection) return;
+
+    const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                    coversSection.classList.toggle('in-viewport', entry.isIntersecting);
+            });
+        }, {threshold: 0.2}
+    );
+        observer.observe(coversSection);
+});
 
 
 
