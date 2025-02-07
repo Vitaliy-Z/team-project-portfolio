7c0191ffe7829a65d459abb23b09f9f4e02e1d52
Marii R <mariia.revak@gmail.com>
2025-02-07 17:45:05 +0200

    section projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,1 +1,27 @@
-console.log('projects');
+const swiper = new Swiper('.swiper', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: true,
+            },
+        });
+
+        
+const button = document.querySelector('.see-project');
+        
+        button.addEventListener('click', function(event) {
+            window.location.href = 'https://github.com/github'; 
+        });
+
+        window.addEventListener('click', function(event) {
+            if (!button.contains(event.target)) {
+                event.stopPropagation(); 
+            }
+        });
+
