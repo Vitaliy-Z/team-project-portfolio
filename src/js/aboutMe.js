5a990353d59dde6f9d7246a2a39c8ca36bb3ef6f
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-02-08 17:15:12 +0200

    swiper

--- src/js/aboutMe.js
+++ src/js/aboutMe.js
@@ -31,15 +31,15 @@ new Swiper('.about-me-skills-list', {
         nextEl: '.about-me-skills-button',
     },
     cssMode: true,
-    breakpoints: {
-        375: {
-            slidesPerView: 6, 
-        },
-        768: {
-            slidesPerView: 6,
-        },
-        1440: {
-            slidesPerView: 6,
-        }, 
-    },
+    // breakpoints: {
+    //     375: {
+    //         slidesPerView: 6, 
+    //     },
+    //     768: {
+    //         slidesPerView: 6,
+    //     },
+    //     1440: {
+    //         slidesPerView: 6,
+    //     }, 
+    // },
 });
