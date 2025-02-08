0480f81f8927c499baff4cec37e19ca2bcca7177
yuliia_mrachynska <mrachynska.y@gmail.com>
2025-02-08 20:16:28 +0300

    update

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,6 @@
 import Accordion from "accordion-js";
 
 const list = document.querySelector(".faq-list");
-const btn = document.querySelectorAll(".faq-btn");
 
   const accordion = new Accordion(list, {
     duration: 400,
@@ -19,10 +18,3 @@ const btn = document.querySelectorAll(".faq-btn");
       }
   });
   
-//   btn.forEach(button => {
-//     button.addEventListener('click', (event) => {
-//         const item = event.target.closest('.faq-container');
-//         item.classList.toggle("is-active");
-//       accordion.toggle(item);
-//     });
-//   });
